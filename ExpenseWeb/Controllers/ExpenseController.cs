dec6628b893aecf5d42bbac279098439cdaf2312
Keoma plovie <ploviekeoma@hotmail.com>
2020-06-11 14:01:21 +0200

    First Change

---
+++ ExpenseWeb/Controllers/ExpenseController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseWeb.Database;
+using ExpenseWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using ExpenseWeb.Domain;
+
+namespace ExpenseWeb.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly IExpenseDatabase _expenseDatabase;
+
+        public ExpenseController(IExpenseDatabase expenseDatabase)
+        {
+            _expenseDatabase = expenseDatabase;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ExpenseCreateViewModel expense)
+        {
+            Expense newExpense = new Expense()
+            {
+                Omschrijving = expense.Omschrijving,
+                Datum = expense.Datum,
+                Bedrag = expense.Bedrag
+            };
+
+            _expenseDatabase.Insert(newExpense);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> expensesFromDb = _expenseDatabase.GetExpenses();
+            List<ExpenseListViewModel> expenses = new List<ExpenseListViewModel>();
+
+            foreach (var expense in expensesFromDb)
+            {
+                expenses.Add(new ExpenseListViewModel { Id = expense.Id, Omschrijving = expense.Omschrijving, Datum = expense.Datum, Bedrag = expense.Bedrag });
+            }
+            return View(expenses);
+        }
+    }
+}
---
+++ ExpenseWeb/Models/ExpenseCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpenseWeb.Models
+{
+    public class ExpenseCreateViewModel
+    {
+        public string Omschrijving { get; set; }
+        public DateTime Datum { get; set; }        
+        public decimal Bedrag { get; set; }
+    }
+        
+}
